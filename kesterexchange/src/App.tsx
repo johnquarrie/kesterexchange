3f2066ad2a65c4091c47284146212db85ab914be
KesterJosh <adeyelukester2@gmail.com>
2025-10-27 10:23:41 +0100

    Coming Soon Update

---
+++ kesterexchange/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='Container'>
+      <div className='Hero'>
+        <h2 className='CenterHead'>Coming Soon</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
